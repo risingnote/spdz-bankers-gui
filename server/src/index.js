cc1196cdc870c711760d2a059d089a536bfb627f
Jonathan Evans <bflat@risingnote.info>
2016-11-30 13:34:09 +0000

    Setup a server to serve gui/provide config.

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,30 @@
 /**
- * See https://github.com/fullstackreact/food-lookup-demo for how to split.
+ * Run a web server to serve the SPDZ gui and a REST endpoint to get gui config at run time.
  */
+'use strict'
+
+const express = require('express')
+const http = require('http')
+const guiConfig = require('../config/spdzGui.json')
+const proxyConfig = require('../config/spdzProxy.json')
+
+const guiPortNum = guiConfig.portNum || '8080'
+const environ = process.env.NODE_ENV || 'development'
+
+const app = express()
+
+// Do not use express to serve GUI in development
+if (process.env.NODE_ENV !== 'development') {
+  app.use(express.static('../../client/build'));
+}
+
+app.get('/config', (req, res) => {
+  res.json(proxyConfig)
+})
+
+app.disable('x-powered-by')
+
+// TODO Allow optional switch to https
+http.createServer(app).listen(guiPortNum, () => {
+  console.log('Serving gui on port ' + guiPortNum + '.')
+})
