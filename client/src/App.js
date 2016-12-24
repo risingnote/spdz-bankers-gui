96e6a8999c5c52fc843d7e018ba1aab6388f88c5
Jonathan Evans <bflat@risingnote.info>
2016-12-24 18:26:43 +0100

    Logo changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
+import ClassNames from 'classnames'
 import cryptologo from './crypto-logo.png'
 import uoblogo from './uob-logo-white-largest.png'
+import spdzlogo from './spdz_logo.svg'
 import './App.css'
 import BankersWithSetup from './bankers_gui/BankersWithSetup'
 
 class App extends Component {
   render() {
+    const spdzLogoClass = ClassNames('App-header-fixed-col', 'App-spdzlogo')
     return (
       <div className="App">
         <header className="App-header">
+          <a title="Bristol Crypt Group SPDZ software" href="https://www.cs.bris.ac.uk/Research/CryptographySecurity/SPDZ/">
+            <img src={spdzlogo} className={spdzLogoClass} alt="SPDZ logo" />
+          </a>        
+          <h2 className="App-header-col">MPC Demonstrators</h2>
           <a title="University of Bristol homepage" href="http://www.bristol.ac.uk">
             <img src={uoblogo} className="App-header-fixed-col" alt="logo" />
-          </a>        
-          <h2 className="App-header-col">SPDZ Demonstrators</h2>
-          <a title="Bristol Cryptography Group" href="http://www.cs.bris.ac.uk/Research/CryptographySecurity/">
-            <img src={cryptologo} className="App-header-fixed-col" alt="logo" />
-          </a>          
+          </a>      
         </header>
         <BankersWithSetup />
         <footer className="App-footer">
@@ -28,7 +31,10 @@ class App extends Component {
             <p>Bristol BS8 1UB UK</p>
             <p>+44 (0)117 331 5663</p>
           </div>
-          <div className="App-footer-col-right">     
+          <div className="App-footer-col-right">  
+            <a title="Bristol Cryptography Group" href="http://www.cs.bris.ac.uk/Research/CryptographySecurity/">
+              <img src={cryptologo} alt="logo" />
+            </a>       
             <a href="http://www.bristol.ac.uk/web/policies/terms-conditions.html#copyright">© 2016 University of Bristol</a>
           </div>
         </footer>
