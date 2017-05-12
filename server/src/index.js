e180ad766df4e5737e7672e7686df618ea6ea142
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-05-12 17:05:10 +0100

    Strip out SSL support. Now in nginx.

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,12 @@
 /**
  * Run a web server to serve the SPDZ gui and a REST endpoint to get gui config at run time.
+ * Expect to be run behind an nginx reverse proxy in prod to provide SSL.
  */
 'use strict'
 
 const express = require('express')
 const http = require('http')
-const https = require('https')
-const fs = require('fs')
-const compression = require('compression')
-const proxyConfig = require('../config/spdzProxy.json')
 const diners = require('./diners')
-const certs = require('../certs/config.json')
-const configForEnv = require('./configForEnv')
 const logger = require('./logging')
 const webRouting = require('./webRouting')
 
@@ -24,21 +19,9 @@ const app = express()
 // Configure web server paths
 webRouting(app)
 
-let webServer
-let guiPortNum
-
 // Configure web server
-if ( certs.https && certs.https === true ) {
-  const httpsOptions = {
-    key: fs.readFileSync(certs.keyFile),
-    cert: fs.readFileSync(certs.certFile)
-  }
-  webServer = https.createServer(httpsOptions, app)
-  guiPortNum = '8443'
-} else {
-  webServer = http.createServer(app)
-  guiPortNum = (environ === 'development') ? '3001' : '8080'
-}
+const webServer = http.createServer(app)
+const guiPortNum = (environ === 'development') ? '3001' : '8080'
 
 // Setup server web socket
 diners.init(webServer)
